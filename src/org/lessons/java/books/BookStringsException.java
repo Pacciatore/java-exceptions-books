ed9e5717903c7c6123e9d4468e20fa2788195021
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-01-31 15:43:42 +0100

    cambio BookStringException eliminando il superfluo

--- src/org/lessons/java/books/BookStringsException.java
+++ src/org/lessons/java/books/BookStringsException.java
@@ -2,15 +2,8 @@ package org.lessons.java.books;
 
 public class BookStringsException extends Exception {
 
-	private String text;
-
-	public BookStringsException(String _text) {
+	public BookStringsException() {
 		super("Stringa vuota non valida!");
-		this.text = _text;
-	}
-
-	public String getText() {
-		return text;
 	}
 
 }
