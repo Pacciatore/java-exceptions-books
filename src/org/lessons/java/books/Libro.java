aac5fcceb037b073d7117b638fa9ff406549bd0f
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-01-31 14:19:19 +0100

    creazione classe Libro con costruttore

---
+++ src/org/lessons/java/books/Libro.java
@@ -0,0 +1,19 @@
+package org.lessons.java.books;
+
+public class Libro {
+
+	private String title;
+	private int numPages;
+	private String author;
+	private String editor;
+
+	public Libro(String _title, int _numPages, String _author, String _editor) {
+
+		this.title = _title;
+		this.numPages = _numPages;
+		this.author = _author;
+		this.editor = _editor;
+
+	}
+
+}
